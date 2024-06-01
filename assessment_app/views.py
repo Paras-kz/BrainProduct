151ca2bebdd07cf0c6b16c123d7073d9e5fe4908
kumar Paras <phenomkps@gmail.com>
2024-06-01 18:24:05 +0530

    initial commit for brain prj

---
+++ assessment_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+from .models import Product, Order
+from .serializers import ProductSerializer, OrderSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
